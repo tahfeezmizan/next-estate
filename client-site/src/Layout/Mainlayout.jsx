3251b0b8ca20be0b76224013c2db30ebd993b4f5
tahfeezmizan <mizanahmed5316@gmail.com>
2025-02-05 12:06:06 +0600

    refector: footer

--- client-site/src/Layout/Mainlayout.jsx
+++ client-site/src/Layout/Mainlayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../pages/Shared/Navbar/Navbar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../pages/Shared/Footer/Footer';
 
 const Mainlayout = () => {
     return (
         <div>
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- client-site/src/pages/Home/Home/Home.jsx
+++ client-site/src/pages/Home/Home/Home.jsx
@@ -1,6 +1,5 @@
 import Properties from "../../Properties/Properties";
 import Blogs from "../../Shared/Blogs/Blogs";
-import Footer from "../../Shared/Footer/Footer";
 import LocationForYou from "../../Shared/LocationForYou/LocationForYou";
 import UserReview from "../../Shared/Reviews/UserReview/UserReview";
 import Advertisement from "../Advertisement/Advertisement";
@@ -15,7 +14,7 @@ const Home = () => {
             <LocationForYou></LocationForYou>
             <UserReview></UserReview>
             <Blogs></Blogs>
-            <Footer></Footer>
+            
         </>
     );
 };
